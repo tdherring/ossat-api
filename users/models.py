51113897dbf4a086492634c17a5f207e6a86b8d5
tdherring <tomherring1999@gmail.com>
2021-07-14 22:15:07 +0100

    Initial setup and implementation of GraphQL authentication API

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(blank=False, max_length=254, verbose_name="email address")
+
+    USERNAME_FIELD = "username"      # e.g: "username", "email"
+    EMAIL_FIELD = "email"         # e.g: "email", "primary_email"
---
+++ users/schema.py
@@ -0,0 +1,3 @@
+import graphene
+from graphene_django import DjangoOpjectType
+from .models import x  # Models to be declared
